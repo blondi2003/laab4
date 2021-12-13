dba2b8f4f5e7d66536a39d268d64be280ba133be
blondi2003 <natash.erschowa2016@yandex.ru>
2021-12-13 10:12:44 +0400

    Добавьте файлы проекта.

---
+++ laab4.2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+    class Program
+    {
+       static double Method (double k)
+       {
+        double m = Math.Pow(k, 2) / 5.0;
+        return m;
+       }
+       static double Result(double k)
+       { 
+         if (k == 0)
+         {
+            return Method(k);
+         }
+         else
+         {
+            return Method(k) + Result(k - 1);
+         }
+       
+       }
+
+       static void Main(string[] args)
+       {
+        int n = Convert.ToInt32(Console.ReadLine());
+        double m=Result(n);
+
+        Console.WriteLine(m);
+        Console.ReadKey();
+       }
+    }
+
---
+++ laab4.3/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static double Method(double k)
+    {
+      double m = k + 1 / 13.0;
+      return m;
+    }
+
+    static double Result(double k)
+    {
+    if(k==-1)
+    {
+            return 1;
+    }
+    else
+    {
+            return Method(k) * Result(k - 1);
+    }
+}
+    static void Main(string[] args)
+    {
+        int k = Convert.ToInt32(Console.ReadLine());
+        double m = Result(k);
+        Console.WriteLine(m);
+        Console.ReadKey();
+    }
+}
